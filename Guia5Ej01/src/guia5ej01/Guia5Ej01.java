fef727b5633927bee9bf632a0ada6ca45292f1ce
fabri <fabricioborsalino@hotmail.com>
2022-09-06 21:45:51 -0300

    Mi primer programa de java

---
+++ Guia5Ej01/src/guia5ej01/Guia5Ej01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia5ej01;
+
+/**
+ *
+ * @author A134192
+ */
+public class Guia5Ej01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
